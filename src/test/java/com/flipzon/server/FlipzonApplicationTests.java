f819b827bbceb84fb8fa41291dd51ec5ae9f3cd2
pavan-varanasi <pavan.varanasi26@gmail.com>
2021-04-18 01:02:07 +0530

    Added Files

---
+++ src/test/java/com/flipzon/server/FlipzonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flipzon.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipzonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
